b25c3f7e983585dd3f29bd922f5e582676ab54f2
wnsdnn <a24369029@gmail.com>
2021-01-11 12:49:24 +0900

    Create login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <div align = center>
+    <span>로그인</span>
+
+        <form method="get" action="login_action.php">
+            <p>ID: <input type="text name="id"></p>
+            <p>PW: <input type="text name="password"></p>
+            <input type="submit" value="로그인">
+        </form>
+        <br>
+        <button id="join" onclick="location">회원가입</button>
+    </div>
+</body>
+</html>
