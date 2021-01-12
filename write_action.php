c63f289c6d96d2108f4ba25cef49c652eb26a9bc
wnsdnn <a24369029@gmail.com>
2021-01-12 10:47:20 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -1,29 +1,30 @@
-<?php 
+<?php
+        $connect = mysqli_connect("localhost", "root", "", "board") or die("fail");
+        
+        $id = $_GET['id'];                      //Writer
+        $pw = $_GET['pw'];                        //Password
+        $title = $_GET['title'];                  //Title
+        $content = $_GET['content'];              //Content
+        $date = date('Y-m-d H:i:s');            //Date
 
-    $connect = mysqli_connect("localhost", "root", "", "board") or die ("fail");
+        $URL = './index.php';                   //return URL
 
-    $id=$_GET['id'];
-    $pw=$_GET['pw'];
-    $email=$_GET['email'];
-    $content = $_GET['content'];
-    $date = date('Y-m-d H:i:s');
 
-    $url = './index.php';
+        $query = "insert into board (number,title, content, date, hit, id, password) 
+                values(null,'$title', '$content', '$date',0, '$id', '$pw')";
 
-    $query = "insert into board(number, title, content, date, hit, id, password) values(null, $'title', $'content', $'date', 0, $'id', $'pw')";
 
-    $result = $connect->$query($query);
-    if($resultl){
-?>         <script>
+        $result = $connect->query($query);
+        if($result){
+?>                 <script>
                 alert("<?php echo "글이 등록되었습니다."?>");
-                location.replace("<?php echo $URL?>")
+                location.replace("<?php echo $URL?>");
             </script>
 <?php
-    } else {
-        echo "FAIL";
-    }
+        }
+        else{
+                echo "FAIL";
+        }
 
-    mysqli_close($connect);
-
-
+        mysqli_close($connect);
 ?>
