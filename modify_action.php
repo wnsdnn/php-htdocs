4dc3a61b57a267c1774c33f6c34bf5319056ce62
wnsdnn <a24369029@gmail.com>
2021-01-12 10:47:11 +0900

    Create modify_action.php

---
+++ modify_action.php
@@ -0,0 +1,19 @@
+<?php
+    $connect = mysqli_connect("localhost", "root", "", "board") or die ("connect fail");
+    $number = $_GET['number'];
+    $title = $_GET['title'];
+    $content = $_GET['content'];
+    $date = date('Y-m-d H:i:s');
+    $query = "update board set title='$title', content='$content', date='$date' where number=$number";
+    $result = $connect->query($query);
+    if($result) {
+?>
+        <script>
+            alert("수정되었습니다.");
+            location.replace("./view.php?number=<?=$number?>");
+        </script>
+<?php    }
+    else {
+        echo "fail";
+    }
+?>
