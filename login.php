46bf2242b79d2a03eff74c41e81b81ee950632f8
wnsdnn <a24369029@gmail.com>
2021-01-12 10:47:04 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
+ 
 <html>
 <head>
-    <title>Document</title>
+        <meta charset='utf-8'>
 </head>
+ 
 <body>
-    <div align = center>
-    <span>로그인</span>
-
-        <form method="get" action="login_action.php">
-            <p>ID: <input type="text name="id"></p>
-            <p>PW: <input type="text name="password"></p>
-            <input type="submit" value="로그인">
+        <div align='center'>
+        <span>로그인</span>
+ 
+        <form method='get' action='login_action.php'>
+                <p>ID: <input name="id" type="text"></p>
+                <p>PW: <input name="pw" type="password"></p>
+                <input type="submit" value="로그인">
         </form>
-        <br>
+        <br />
         <button id="join" onclick="location.href='./join.php'">회원가입</button>
-    </div>
+ 
+        </div>
+ 
+ 
 </body>
+ 
 </html>
